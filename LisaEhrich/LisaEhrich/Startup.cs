67a83bc7a19807805932249b73b3034c73cec9c2
tosca ragnini <tosca.ragnini@gmail.com>
2017-09-25 22:15:45 -0500

    remove unused files and methods

--- LisaEhrich/LisaEhrich/Startup.cs
+++ LisaEhrich/LisaEhrich/Startup.cs
@@ -14,11 +14,7 @@ namespace LisaEhrich
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        
-        public void ConfigureServices(IServiceCollection services)
-        {
-        
-        }
+         
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
