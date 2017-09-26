712d61e87a65d9176dab566259abd91638816152
tosca ragnini <tosca.ragnini@gmail.com>
2017-09-25 21:39:19 -0500

    add leaves images

--- LisaEhrich/LisaEhrich/Startup.cs
+++ LisaEhrich/LisaEhrich/Startup.cs
@@ -14,37 +14,11 @@ namespace LisaEhrich
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
+        
         }
-   /*     internal class ConfigurationBuilder
-        {
-            public ConfigurationBuilder()
-            {
-            }
-        }
-
-        internal object SetBasePath(string ContentRootPath)
-        {
-            throw new NotImplementedException();
-        }
-
-
-       public Startup(IHostingEnvironment env)
-        {
-
-
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            string sAppPath = env.ContentRootPath; //Application Base Path
-            string swwwRootPath = env.WebRootPath;  //wwwroot folder path
-            ConfigurationBuilder = builder.Build();
-        }*/
-
-
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
