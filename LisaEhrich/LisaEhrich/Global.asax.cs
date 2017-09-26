33c397eae95a853466810c8b9142303d3c42635a
tosca ragnini <tosca.ragnini@gmail.com>
2017-09-25 22:41:02 -0500

    save sln

--- LisaEhrich/LisaEhrich/Global.asax.cs
+++ LisaEhrich/LisaEhrich/Global.asax.cs
@@ -12,6 +12,8 @@ namespace LisaEhrich
     {
         protected void Application_Start()
         {
+
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
